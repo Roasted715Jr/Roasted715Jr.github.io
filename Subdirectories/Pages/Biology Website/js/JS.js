52229df420b7bc5c588c3b7402f248e5ec3ad15b
Matthew Duphily <matthew.duphily@live.com>
2017-03-27 17:30:40 -0400

    Added JS to make changing tab names easier
    Honestly, it would have probably been easier just throwing it straight
    into the  HTML, but I did learn a lot from not taking the easy way out.

--- Subdirectories/Pages/Biology Website/js/JS.js
+++ Subdirectories/Pages/Biology Website/js/JS.js
@@ -1,9 +1,12 @@
 var main = function () {
 
-	var tab1 = "lul";
-
-	document.getElementById("tab1").innerHTML = tab1;
-
+	var tabNames = ["Home", "How Its Used", "Pros/Cons", "My Question", "Works Cited"];
+	
+	document.getElementById("tab1").innerHTML = tabNames[0];
+	document.getElementById("tab2").innerHTML = tabNames[1];
+	document.getElementById("tab3").innerHTML = tabNames[2];
+	document.getElementById("tab4").innerHTML = tabNames[3];
+	document.getElementById("tab5").innerHTML = tabNames[4];
 };
 
 $(document).ready(main);
