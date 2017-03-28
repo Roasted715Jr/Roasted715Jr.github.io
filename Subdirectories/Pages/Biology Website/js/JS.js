1411ffcf1e897e4fc72cc7784f8de2fa35b2875b
Matthew Duphily <matthew.duphily@live.com>
2017-03-28 15:39:21 -0400

    Coincidentally found out how to do the "for" loop after I presented... KMS

--- Subdirectories/Pages/Biology Website/js/JS.js
+++ Subdirectories/Pages/Biology Website/js/JS.js
@@ -1,12 +1,10 @@
 var main = function () {
 
 	var tabNames = ["Home", "How Its Used", "Pros/Cons", "My Question", "Works Cited"];
-	
-	document.getElementById("tab1").innerHTML = tabNames[0];
-	document.getElementById("tab2").innerHTML = tabNames[1];
-	document.getElementById("tab3").innerHTML = tabNames[2];
-	document.getElementById("tab4").innerHTML = tabNames[3];
-	document.getElementById("tab5").innerHTML = tabNames[4];
+
+	for (var counter = 1; counter <= tabNames.length; counter++) {
+		document.getElementById("tab" + counter).innerHTML = tabNames[counter - 1];
+	}
 };
 
 $(document).ready(main);
