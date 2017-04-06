1e0eae9557f93cc5f821b1fba56a6c0bb839df64
Matthew Duphily <matthew.duphily@live.com>
2017-04-05 21:02:53 -0400

    Started adding a programs directory

--- Subdirectories/Pages/Biology Website/js/JS.js
+++ Subdirectories/Pages/Biology Website/js/JS.js
@@ -2,7 +2,8 @@ var main = function () {
 
 	var tabNames = ["Home", "How Its Used", "Pros/Cons", "My Question", "Works Cited"];
 
-	for (var counter = 1; counter <= tabNames.length; counter++) {
+	//We do "counter < tabNames.length" since naturally the length will be 1 greater than the highest index # in the array
+	for (var counter = 1; counter < tabNames.length; counter++) {
 		document.getElementById("tab" + counter).innerHTML = tabNames[counter - 1];
 	}
 };
---
+++ Subdirectories/Programs/js/ContactListWebJquery.js
@@ -0,0 +1,68 @@
+var main = function() {
+
+	var answer = prompt("What would you like to do?\nAdd a contact\nRemove a contact\nList all your contacts\nSearch for a contact\n");
+	
+	var actions = ["add a contact", "remove a contact", "list all your contacts", "search for a contact"];
+	
+	answer = answer.toLowerCase();
+	//console.log(answer);
+	for (var key in actions){
+		if (answer = actions[key]) {
+			console.log(key);
+			console.log("User has answered");
+		}
+	}
+	
+	//Create a class "Contact" with properties of "firstName", "lastName", "phoneNumber", "email", and "address"
+	function Contact(firstName, lastName, phoneNumber, email, address) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.address = address;
+	}
+	
+	//Create an array to hold all of our contacts
+	var contacts = [];
+	
+	//Create a function to show our whole contacts list
+	var listContacts = function() {
+		for (var key in contacts) {
+			//"parseInt()" is used to parse a string as a number since "key" is a string
+			var x = parseInt(key) + 1;
+			console.log("Contact #" + x + ": " + contacts[key].firstName + " " + contacts[key].lastName +
+					"\nPhone Number: " + contacts[key].phoneNumber + "\nEmail: " + contacts[key].email +
+					"\nAddress: " + contacts[key].address + "\n");
+		}
+	};
+	
+	var search = function(lastName) {
+	    var contactsLength = contacts.length;
+	    for (var i = 0; i < contactsLength; i++) {
+	        if (lastName === contacts[i].lastName) {
+	            printPerson(contacts[i]);
+	        }
+	    }
+	};
+	
+	function printContact(person) {
+	    console.log(person.firstName + " " + person.lastName);
+	}
+	
+	//Create a function to add someone to our contacts array
+	var addToContacts = function(person) {
+		//We do "contacts.length" because this number will always be exactly 1 higher than the greatest index in the array
+		contacts[contacts.length] = person;
+	};
+	
+	var add = function(firstName, lastName, email, phoneNumber, address) {
+	    this.firstName = firstName;
+	    this.lastName = lastName;
+	    this.email = email;
+	    this.phoneNumber = phoneNumber;
+	    this.address= address
+	    addToContacts(this);
+	};
+}
+
+$(document).ready(main);
