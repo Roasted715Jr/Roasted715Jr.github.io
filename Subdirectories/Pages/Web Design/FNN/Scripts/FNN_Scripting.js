edd6e00bb7fdcb68db882794df891d67eb2f308a
Roasted715Jr <matdup516@cr.k12.de.us>
2017-11-07 10:14:47 -0500

    Added ECS website design

---
+++ Subdirectories/Pages/Web Design/FNN/Scripts/FNN_Scripting.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+var main = function () {
+    var topArticle = Math.floor(Math.random() * 20); /*Make a random whole number by making a random float
+	< 1, multiply it by 20, then round it down to a whole number*/
+	
+	/*$(".logo").click(function() {
+		$("body").animate({
+			right: '300px'
+		}, 200);
+	});*/
+}
+
+$(document).ready(main);
